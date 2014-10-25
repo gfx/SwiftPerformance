fd2b852ff805cd42aa6d14f98bea62ca914aee29
FUJI Goro (gfx) <gfuji@cpan.org>
2014-10-25 13:23:17 +0900

    initial commit

--- SwiftPerformance/ViewController.swift
+++ SwiftPerformance/ViewController.swift
@@ -8,18 +8,69 @@
 
 import UIKit
 
+class C {
+    let foo = "foo"
+    let bar = "bar"
+    let baz = "baz"
+
+    init() {}
+    deinit {}
+}
+class S {
+    let foo = "foo"
+    let bar = "bar"
+    let baz = "baz"
+
+    init() {}
+}
+
 class ViewController: UIViewController {
+
+    var N = 100000
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
+    @IBAction func startTiming(sender: UIButton) {
+        perform("class ") {
+            var a = [C]()
+            for _ in 1 ... self.N {
+                a.append(C())
+            }
+        }
+        perform("struct") {
+            var a = [S]()
+            for _ in 1 ... self.N {
+                a.append(S())
+            }
+        }
+    }
+
+    func perform(name: String, block: () -> Void) {
+        var results = [NSTimeInterval]()
+
+        for _ in 1 ... 10 {
+            let t0 = getTime()
+            block()
+            results.append(getTime() - t0)
+        }
+
+        let sum = results.reduce(0.0) { (a, item) in
+            a + item
+        }
+        let avg = sum / Double(results.count)
+
+        let dev = sqrt(results.reduce(0.0) { (a, item) in
+            a + pow(item - avg, 2)
+        } / Double(results.count))
+
+        println(String(format: "%@: %.03f (+/- %.03f)", name, avg, dev))
+    }
+
+    func getTime() -> NSTimeInterval {
+        return NSDate().timeIntervalSince1970;
+    }
 }
 
